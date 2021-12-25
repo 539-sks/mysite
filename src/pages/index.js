8033bb60385ac18e55f6433f3b2b7eee4286efe9
539-sks <kosakase539@gmail.com>
2021-12-25 11:36:06 +0900

    ヘッダー文言変更

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
 	</figure>
 	<div class="catch">
 		<h1>There is no love sincerer than<br /> the love of food.</h1>
-		<p>食物を愛するよりも誠実な愛はない ― バーナード・ショー</p>
+		<p>食物を愛するよりも誠実な愛はない ― バーナード・ショー!!</p>
 	</div>
 	<div class="wave">
 		<img src="/images/wave.svg" alt="" />
