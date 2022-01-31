ae714972b9c6b323e0d9367e02122e7f62376721
539-sks <kosakase539@gmail.com>
2022-01-31 14:00:19 +0900

    DONE STEP:4-2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,114 +1,114 @@
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import React from "react"
+import Layout from "../components/layout"
 
 const name = "Gatsbyyy"
 
-export default function Home() {
+export default function Home({ data }) {
   return (
-    <div>
-<header class="header">
-	<div class="container">
-		<div class="site">
-			<a href="base-index.html">
-				<img src="/images/logo.svg" alt="ESSENTIALS" />
-			</a>
-		</div>
-		
-		<nav class="nav">
-			<ul>
-				<li><a href="base-index.html">TOP</a></li>
-				<li><a href="base-about.html">ABOUT</a></li>
-			</ul>
-		</nav>
-	</div>
-</header>
-
-<section class="hero">
-	<figure>
-		<img src="/images/hero.jpg" alt="" />
-	</figure>
-	<div class="catch">
-		<h1>There is no love sincerer than<br /> the love of food.</h1>
-		<p>食物を愛するよりも誠実な愛はない ― バーナード・ショー!!</p>
-	</div>
-	<div class="wave">
-		<img src="/images/wave.svg" alt="" />
-	</div>
-</section>
+   <Layout>
+     <section class="hero">
+        <figure>
+          <Img fluid={data.hero.childImageSharp.fluid}
+               alt="" 
+               style={{ height: "100%" }}
+          />
+        </figure>
+        <div class="catch">
+          <h1>There is no love sincerer than<br /> the love of food.</h1>
+          <p>食物を愛するよりも誠実な愛はない ― バーナード・ショー!!</p>
+        </div>
+        <div class="wave">
+          <img src="/images/wave.svg" alt="" />
+        </div>
+      </section>
 
 
-<section class="food">
-	<div class="container">
-		<h2 class="bar">Food <span>Essence</span></h2>
+      <section class="food">
+        <div class="container">
+          <h2 class="bar">Food <span>Essence</span></h2>
 
-		<div class="details">
-			<div class="detail">
-				<figure>
-					<img src="/images/fruit.jpg" alt="" />
-				</figure>
-				<h3>フルーツ</h3>
-				<p>FRUIT</p>
-				<p>甘くてすっぱくておいしい果実たち。<br />旬のフルーツを満喫します。</p>
-			</div>
+          <div class="details">
+            <div class="detail">
+              <figure>
+                <Img fluid={data.fruit.childImageSharp.fluid} alt="" />
+              </figure>
+              <h3>フルーツ</h3>
+              <p>FRUIT</p>
+              <p>甘くてすっぱくておいしい果実たち。<br />旬のフルーツを満喫します。</p>
+            </div>
 
-			<div class="detail">
-				<figure>
-					<img src="/images/grain.jpg" alt="" />
-				</figure>
-				<h3>穀物</h3>
-				<p>GRAIN</p>
-				<p>食事の基本となる穀物。<br />毎日の活動のエネルギー源になります。</p>
-			</div>
+            <div class="detail">
+              <figure>
+                <Img fluid={data.grain.childImageSharp.fluid} alt="" />
+              </figure>
+              <h3>穀物</h3>
+              <p>GRAIN</p>
+              <p>食事の基本となる穀物。<br />毎日の活動のエネルギー源になります。</p>
+            </div>
 
-			<div class="detail">
-				<figure>
-					<img src="/images/beverage.jpg" alt="" />
-				</figure>
-				<h3>飲み物</h3>
-				<p>BEVERAGE</p>
-				<p>リラックスするのに欠かせない飲み物。<br />お気に入りの一杯はありますか？</p>
-			</div>
-		</div>
-	</div>
-</section>
+            <div class="detail">
+              <figure>
+                <Img fluid={data.beverage.childImageSharp.fluid} alt="" />
+              </figure>
+              <h3>飲み物</h3>
+              <p>BEVERAGE</p>
+              <p>リラックスするのに欠かせない飲み物。<br />お気に入りの一杯はありますか？</p>
+            </div>
+          </div>
+        </div>
+      </section>
 
-<section class="photo">
-	<h2 class="sr-only">Photo</h2>
-	<figure>
-		<img src="/images/berry.jpg" alt="赤く熟したベリー" />
-	</figure>
-</section>
-
-<footer class="footer">
-	<div class="container">
-		<div class="site">
-		<a href="base-index.html">
-			<img src="/images/logo-w.svg" alt="ESSENTIALS" />
-			<p>おいしい食材と食事を探求するサイト</p>
-		</a>
-		</div>
-
-		<ul class="sns">
-			<li>
-				<a href="https://twitter.com/">
-					<i class="fab fa-twitter"></i>
-					<span class="sr-only">Twitter</span>
-				</a>
-			</li>
-			<li>
-				<a href="https://facebook.com/">
-					<i class="fab fa-facebook-square"></i>
-					<span class="sr-only">Facebook</span>
-				</a>
-			</li>
-			<li>
-				<a href="http://instagram.com/">
-					<i class="fab fa-instagram"></i>
-					<span class="sr-only">Instagram</span>
-				</a>
-			</li>
-		</ul>
-	</div>
-</footer>
-    </div>
+      <section class="photo">
+        <h2 class="sr-only">Photo</h2>
+        <figure>
+          <Img fluid={data.berry.childImageSharp.fluid}
+               alt="" 
+               style={{ height: "100%" }}
+          />
+        </figure>
+      </section>
+   </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    hero: file(relativePath: {eq: "hero.jpg"}) {
+      childImageSharp {
+        fluid(maxWidth: 1600) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    fruit: file(relativePath: {eq: "fruit.jpg"}) {
+      childImageSharp {
+        fluid(maxWidth: 320) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    grain: file(relativePath: {eq: "grain.jpg"}) {
+      childImageSharp {
+        fluid(maxWidth: 320) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    beverage: file(relativePath: {eq: "beverage.jpg"}) {
+      childImageSharp {
+        fluid(maxWidth: 320) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    berry: file(relativePath: {eq: "berry.jpg"}) {
+      childImageSharp {
+        fluid(maxWidth: 1600) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
